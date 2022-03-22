a7a82dd90efb497089b789b9ddfaa89f62a81099
RamziA961 <60425857+RamziA961@users.noreply.github.com>
2022-03-22 09:47:52 +0200

    Initial Commit

---
+++ VibTester/VibTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibTesterApp.swift
+//  VibTester
+//
+//  Created by Ramzi Abou Chahine on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct VibTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
